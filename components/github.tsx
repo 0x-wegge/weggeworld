8665131c13d05a46a899338ccf2425ce006c9ae3
wegge <93815061+0xwegge@users.noreply.github.com>
2022-05-25 19:41:26 -0700

    missed a spot

--- components/github.tsx
+++ components/github.tsx
@@ -6,7 +6,7 @@ function Github() {
         <Grid lg={12} xl={12} justify="center" alignContent="center">
           <Card width="100%" margin="25px">
             <Text h4 my={0}>Github</Text>
-            <Link target="_blank" href="https://github.com/0x-wegge">
+            <Link target="_blank" href="https://github.com/0xwegge">
               <Icon icon="icon-park:github" width="64" height="64"></Icon>
             </Link>
             <Grid xs={0} sm={0} md={0} lg={12} xl={12}>
