33d01129424412e7c45312dba8018903bbcf8d2b
wegge <93815061+0xwegge@users.noreply.github.com>
2022-05-25 19:41:08 -0700

    update medium name

--- components/medium.tsx
+++ components/medium.tsx
@@ -6,12 +6,12 @@ function Medium() {
         <Grid lg={12} xl={12} justify="center" alignContent="center">
           <Card width="95%" margin="25px">
             <Text h4 my={0}>Medium</Text>
-            <Link color target="_blank" href="https://medium.com/@0x_wegge">
+            <Link color target="_blank" href="https://medium.com/@0xwegge">
               <Icon icon="icon-park:abnormal" width="64" height="64"></Icon>
             </Link>
             <Grid xs={0} sm={0} md={0} lg={12} xl={12}>
             <Card.Footer>
-              <Link color target="_blank" href="https://medium.com/@0x_wegge">https://medium.com/@0x_wegge</Link>
+              <Link color target="_blank" href="https://medium.com/@0xwegge">https://medium.com/@0xwegge</Link>
             </Card.Footer>
             </Grid>
           </Card>
